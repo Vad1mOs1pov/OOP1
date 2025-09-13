3b4d6d17488e49f1b807a0a9dc2c75bb02a2d14a
VadimOsipov <bpandab.ru@gmail.com>
2025-09-13 13:25:26 +0700

    ыыыыыы

---
+++ WindowsFormsApplication1/Model/Customers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Customers
+    {
+        private int id;
+        private string fullname;
+        private string address;
+
+        public string Name
+        {
+            get
+            {
+                return fullname;
+            }
+            set
+            {
+                if (value.Length > 200)
+                    throw new ArgumentException("Длина поля превышает 200 символов.");
+                fullname = value;
+            }
+        }
+
+        public string Adress
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                if (value.Length > 500)
+                    throw new ArgumentException("Длина поля превышает 500 символов.");
+                address = value;
+            }
+        }
+        public Customers() { }
+
+    }
+}
---
+++ WindowsFormsApplication1/Model/Item.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Model
+{
+    internal class Item
+    {
+        private int id;
+        private string name;
+        private string info;
+        private float cost;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value.Length > 200)
+                    throw new ArgumentException("Длина поля превышает 200 символов.");
+                name = value;
+            }
+        }
+
+        public string Info
+        {
+            get
+            {
+                return info;
+            }
+            set
+            {
+                if (value.Length > 1000)
+                    throw new ArgumentException("Длина поля превышает 1000 символов.");
+                info = value;
+            }
+        }
+
+        public float Cost
+        {
+            get
+            {
+                return cost;
+            }
+            set
+            {
+                if (value < 0 || value > 100000)
+                    throw new ArgumentOutOfRangeException(nameof(cost));
+    
+                    cost = value;
+            }
+        }
+        public Item() { }
+    }
+}
